137b2c52b995d17c248ef24db41e3fe90e259c8f
Sabarinath049 <teja@paymatrix.co.in>
2021-11-19 15:06:10 +0530

    api call for posts, implementing cards

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
+import React, { useEffect, useState } from "react";
 import 'tailwindcss/tailwind.css'
+import { Layout } from "../components/index";
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,8 @@
-import Head from 'next/head'
-import { PostCard, Categories, PostWidget } from '../components/index'
+import Head from "next/head";
+import { PostCard, Categories, PostWidget } from "../components/index";
+import { getPosts } from "../services/index";
 
-const posts = [
-  {
-    title: "Testing 1",
-    excerpt: "Testing Excerpt 1"
-  },
-  {
-    title: "Testing 2",
-    excerpt: "Testing Excerpt 2"
-  }
-];
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -21,7 +11,9 @@ export default function Home() {
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-log-1">
-          { posts.map((post) => <PostCard post={post} key={post.title} />) }
+          {posts.map((post) => (
+            <PostCard post={post.node} key={post.title} />
+          ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
@@ -31,5 +23,13 @@ export default function Home() {
         </div>
       </div>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
 }
